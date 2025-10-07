77e2c07d9a0a4f2067e64e3112a818cd58e0350a
Juan Fernando Rosero <128848026+SweetArctik@users.noreply.github.com>
2025-10-06 20:07:19 -0500

    Eliminar carpetas vacías

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -11,16 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Intro con integrantes y tema */}
         <Route path="/intro" element={<PresentationIntro />} />
-        
-        {/* Presentación principal */}
         <Route path="/app" element={<App />} />
-
-        {/* Iniciar SIEMPRE en App */}
         <Route path="/" element={<Navigate to="/app" replace />} />
-
-        {/* Fallback */}
         <Route path="*" element={<Navigate to="/app" replace />} />
       </Routes>
     </BrowserRouter>
